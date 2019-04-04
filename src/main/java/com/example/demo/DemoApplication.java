c015a58b81ac702b081e8489e5d4b789ae8e4e61
John Doe <johndoe@example.com>
2019-04-04 18:59:43 +0800

    chapter8

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
 
+        String s = "abcd";
+        s = s + 1;
+        System.out.print(s);// result : abcd1
+    }
 }
