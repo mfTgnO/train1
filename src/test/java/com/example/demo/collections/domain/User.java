c78536f55c30f1d7cff895b83feb8b6a4134e8be
John Doe <johndoe@example.com>
2019-06-05 14:24:21 +0800

    test commit change

---
+++ src/test/java/com/example/demo/collections/domain/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.collections.domain;
+
+/**
+ * @package: com.example.demo.collections.domain
+ * @author:
+ * @email:
+ * @createDate: 2019-06-05 10:16
+ * @description:
+ */
+public class User {
+}
