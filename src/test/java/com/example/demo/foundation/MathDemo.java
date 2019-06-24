2fc600cf2ef426de4527f873aaca624c767fd1ca
John <johndoe1@example.com>
2019-06-24 16:47:23 +0800

    math demo

---
+++ src/test/java/com/example/demo/foundation/MathDemo.java
@@ -0,0 +1,18 @@
+package com.example.demo.foundation;
+
+import org.junit.Test;
+
+/**
+ * @package: com.example.demo.foundation
+ * @author:
+ * @email:
+ * @createDate: 2019-06-24 11:03
+ * @description:
+ */
+public class MathDemo {
+    @Test
+    public void test1() {
+        long round = Math.round(-1.5);
+        System.out.println(round);
+    }
+}
