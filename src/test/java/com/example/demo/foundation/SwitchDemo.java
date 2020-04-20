75e4783bc9da790ba4462d3dbf60102fc7256deb
wjh <wjh@w.com>
2020-04-20 09:56:54 +0800

    switch statement in java

--- src/test/java/com/example/demo/foundation/SwitchDemo.java
+++ src/test/java/com/example/demo/foundation/SwitchDemo.java
@@ -28,4 +28,32 @@ public class SwitchDemo {
         }
         System.out.println("end");
     }
+
+    /**
+     * switch statement in java
+     */
+    @Test
+    public void Test2() {
+        // char grade = args[0].charAt(0);
+        // char grade = 'C';
+        char grade = 'B';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Well done");
+                break;
+            case 'D':
+                System.out.println("You passed");
+            case 'F':
+                System.out.println("Better try again");
+                break;
+            default:
+                System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
 }
